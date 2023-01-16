81df6fad6eafad42557d0a99d63bef094180a54c
codewithsanjeev <codewithsanjeev@gmail.com>
2023-01-16 22:39:59 +0530

    Question 02 Solved

---
+++ js/JSQuestionsAndSolutions/02/mydetails.js
@@ -0,0 +1,8 @@
+let firstName = "Sanjeev"
+let middleName = "Pratap"
+let lastName = "Singh"
+let maritalStatus = "Married"
+let country = "India"
+let age = 30
+
+console.log(`Hey My name is ${firstName} ${middleName} ${lastName} and I got ${maritalStatus} in 2015. Rightnow, I,m living in ${country} and my age is ${age}.`);
