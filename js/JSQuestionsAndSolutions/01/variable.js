8f1cf2154e378098d96e6b20c37b05f4fb54d2dd
codewithsanjeev <codewithsanjeev@gmail.com>
2023-01-16 22:33:56 +0530

    Question 01 Solved

---
+++ js/JSQuestionsAndSolutions/01/variable.js
@@ -0,0 +1,12 @@
+let valueOne = 840;
+let valueTwo = "Sanjeev";
+let youWillDie = true;
+let neverTry = undefined;
+let lifeWithLove = null;
+
+console.log(`Let's start with ${valueTwo} and he has ${valueOne} and he 
+wants to something ${youWillDie} but his life is ${neverTry} and he got ${lifeWithLove}`);
+
+console.log(`Let's start with ${typeof (valueTwo)} and he has ${typeof (valueOne)} and he 
+wants to something ${typeof (youWillDie)} but his life is ${typeof (neverTry)} and he got ${typeof (lifeWithLove)}`);
+
