a30a922375b1e064d516f442b366c9b04098d263
codewithsanjeev <codewithsanjeev@gmail.com>
2023-01-18 15:25:39 +0530

    Q.10 Solved => Nice test

---
+++ js/JSQuestionsAndSolutions/10/valuecampare.js
@@ -0,0 +1,34 @@
+/* 
+    - 4 > 3  => True
+    - 4 >= 3 => True
+    - 4 < 3  => False
+    - 4 <= 3 => False
+    - 4 == 4 => True
+    - 4 === 4 => True
+    - 4 != 4 => False
+    - 4 !== 4 => False
+    - 4 != '4' => False
+    - 4 == '4' => True
+    - 4 === '4' => False
+    - Find the length of python and jargon and make a falsy comparison statement.
+*/
+
+console.log(4 > 3);
+console.log(4 >= 3);
+console.log(4 < 3);
+console.log(4 <= 3);
+console.log(4 == 4);
+console.log(4 === 4);
+console.log(4 != 4);
+console.log(4 !== 4);
+console.log(4 != '4');
+console.log(4 == '4');
+console.log(4 === '4');
+
+// Checking out these two words for falsy values
+console.log("Checking out these two words");
+
+let wordOne = "python";
+let wordTwo = "jargon";
+
+console.log(wordOne == wordTwo);
