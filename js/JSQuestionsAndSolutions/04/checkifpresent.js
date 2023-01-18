c1021cf786d64721507f11e5181af9ea55543f00
codewithsanjeev <codewithsanjeev@gmail.com>
2023-01-18 14:39:02 +0530

    Question 04 Resolved

---
+++ js/JSQuestionsAndSolutions/04/checkifpresent.js
@@ -0,0 +1,7 @@
+let checkData = " First of all, we are going to some scripts in upcoming projects and for that here i'm trying get the access of it.";
+
+// find word "Script" in inside this string
+
+let find = checkData.includes("script");
+
+console.log(find);
