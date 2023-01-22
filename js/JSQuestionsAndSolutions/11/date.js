8bc7b1bdd5981cd2eeccb2599c21f099a3d16e39
codewithsanjeev <codewithsanjeev@gmail.com>
2023-01-22 22:05:57 +0530

    Added A Few Things and Folder

---
+++ js/JSQuestionsAndSolutions/11/date.js
@@ -0,0 +1,8 @@
+let dateToday = new Date();
+console.log(`Let's Print Date Today: ${dateToday}`);
+console.log(`Year: ${dateToday.getFullYear()}`);
+console.log(`Month:  ${dateToday.getMonth() + 1}`);
+console.log(`Date:  ${dateToday.getDate()}`);
+console.log(`Day: ${dateToday.getDay() + 1}`);
+console.log(`Hours: ${dateToday.getHours()}`);
+console.log(`Minutes: ${dateToday.getMinutes()}`);
